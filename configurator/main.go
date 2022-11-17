c510cc86f39a53de25a97c3e217f92f2dc57ce84
tataki26 <tataki26@gmail.com>
2022-11-17 09:45:49 +0900

    Add data structure and scanProcess form
    - create initial layout of the ecat configurator program

--- configurator/main.go
+++ configurator/main.go
@@ -6,6 +6,20 @@ import (
 	"net/http"
 )
 
+type device struct {
+	ErrorCode int
+	Name      string
+	Count     int
+	Info      info
+}
+
+type info struct {
+	ProductName string
+	ProductCode int
+	RevisionNo  int
+	VendorId    string
+}
+
 var tpl *template.Template
 
 func init() {
@@ -30,7 +44,10 @@ func index(w http.ResponseWriter, req *http.Request) {
 
 func scan(w http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
-		err := tpl.ExecuteTemplate(w, "scanProcess.gohtml", nil)
+		info := info{"MADLN05BE", 4885, 10000, "66FF"}
+		device := device{0, "Device 0", 1, info}
+
+		err := tpl.ExecuteTemplate(w, "scanProcess.gohtml", device)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			log.Fatalln(err)
