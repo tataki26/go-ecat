7887b7bb00483a3edc6012677b59a04235309204
tataki26 <tataki26@gmail.com>
2022-11-17 10:42:52 +0900

    Update data structure and apply it to the form
    - add profile structure and update device and info structure
    - pass profile structure to the form
    - static data

--- configurator/main.go
+++ configurator/main.go
@@ -6,14 +6,20 @@ import (
 	"net/http"
 )
 
+type profile struct {
+	ProfileNo  int
+	ErrorCode  int
+	Count      int
+	DeviceList []device
+}
+
 type device struct {
-	ErrorCode int
-	Name      string
-	Count     int
-	Info      info
+	Name string
+	Info info
 }
 
 type info struct {
+	VendorName  string
 	ProductName string
 	ProductCode int
 	RevisionNo  int
@@ -44,10 +50,47 @@ func index(w http.ResponseWriter, req *http.Request) {
 
 func scan(w http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodPost {
-		info := info{"MADLN05BE", 4885, 10000, "66FF"}
-		device := device{0, "Device 0", 1, info}
+		d0 := device{
+			Name: "Device0",
+			Info: info{
+				VendorName:  "eMotionTek",
+				ProductName: "UNI_ECAT60B_2X",
+				ProductCode: 2003,
+				RevisionNo:  1,
+				VendorId:    "AAA555",
+			},
+		}
 
-		err := tpl.ExecuteTemplate(w, "scanProcess.gohtml", device)
+		d1 := device{
+			Name: "Device1",
+			Info: info{
+				VendorName:  "eMotionTek",
+				ProductName: "UNI_ECAT60B_1X",
+				ProductCode: 2002,
+				RevisionNo:  1,
+				VendorId:    "AAA555",
+			},
+		}
+
+		d2 := device{
+			Name: "Device2",
+			Info: info{
+				VendorName:  "FASTECH",
+				ProductName: "Ezi-SERVO2 EtherCAT",
+				ProductCode: 1002,
+				RevisionNo:  1,
+				VendorId:    "0FA0",
+			},
+		}
+
+		p := profile{
+			ProfileNo:  402,
+			ErrorCode:  0,
+			Count:      3,
+			DeviceList: []device{d0, d1, d2},
+		}
+
+		err := tpl.ExecuteTemplate(w, "scanProcess.gohtml", p)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			log.Fatalln(err)
